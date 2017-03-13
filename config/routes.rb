03a4e0e7d295b76421a2d02b954c75a6d9f1b9f4
Danira Karovic <karovic.danira@gmail.com>
2017-03-13 23:53:33 +0100

    changed the route file

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root 'responsive_page#index'
+  root 'landing_page#landingpage'
   get '/contact' => 'responsive_page#contact', as: :contact
   get '/catalog' => 'responsive_page#catalog', as: :catalog
   get '/landingpage'=> 'landing_page#landingpage', as: :landingpage
