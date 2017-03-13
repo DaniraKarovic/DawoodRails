9b4777f3514eb94dae3633283c82610837d84677
Danira Karovic <karovic.danira@gmail.com>
2017-03-14 00:11:55 +0100

    remove windows

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
   root 'landing_page#landingpage'
-  get '/contact' => 'responsive_page#contact', as: :contact
-  get '/catalog' => 'responsive_page#catalog', as: :catalog
-  get '/landingpage'=> 'landing_page#landingpage', as: :landingpage
+  get '/'=> 'landing_page#landingpage', as: :landingpage
 end
